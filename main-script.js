2fe47778304199be184f5ea1a743860395536c70
Tomer Gal <tomergal04@gmail.com>
2023-07-27 12:24:05 +0300

    added

--- main-script.js
+++ main-script.js
@@ -1,3 +1,25 @@
+const modal = document.getElementById('formModal');
+const closeBtn = modal.querySelector('.close');
+const modalDisplay = document.querySelector('.modal-content');
+
+function openFormModal() {
+  const storedContactData = JSON.parse(localStorage.getItem('contact'));
+
+  document.getElementById('modalFullName').textContent = storedContactData.fullName || '';
+  document.getElementById('modalEmail').textContent = storedContactData.email || '';
+  document.getElementById('modalPhone').textContent = storedContactData.phone || '';
+  document.getElementById('modalMessage').textContent = storedContactData.message || '';
+
+  modal.style.display = 'block';
+}
+
+function closeModal() {
+  modal.style.display = 'none';
+  modalDisplay.style.display = 'none';
+}
+
+closeBtn.addEventListener('click', closeModal);
+
 function submitForm(event) {
   event.preventDefault(); 
   const fullName = document.getElementById('fullNameInput').value;
@@ -22,26 +44,3 @@ function submitForm(event) {
   document.getElementById('messageInput').value = '';
 }
 
-const modal = document.getElementById('formModal');
-const closeBtn = modal.querySelector('.close');
-const modalDisplay = document.querySelector('.modal-content')
-function openFormModal() {
-
-  const storedContactData = JSON.parse(localStorage.getItem('contact'));
-
-  document.getElementById('modalFullName').textContent = storedContactData.fullName || '';
-  document.getElementById('modalEmail').textContent = storedContactData.email || '';
-  document.getElementById('modalPhone').textContent = storedContactData.phone || '';
-  document.getElementById('modalMessage').textContent = storedContactData.message || '';
-
-  modal.style.display = 'block';
-}
-closeBtn.addEventListener('click', closeModal);
-
-function closeModal() {
-  modal.style.display = 'none' ;
-  modalDisplay.style.display = ' none !important;' ;
-}
-if (localStorage.getItem('contact')) {
-  openFormModal();
-}
